dfb49f376bd1978a4c249d9c5febd69bf0ba5893
Sanjayraj2003 <113586066+Sanjayraj2003@users.noreply.github.com>
2024-02-09 11:44:30 +0530

    webite completed

---
+++ src/content_option.js
@@ -0,0 +1,120 @@
+const logotext = "Sanjay";
+const meta = {
+    title: "Sanjayraj",
+    description: "I’m Sanjayraj data scientist _ Full stack devloper,currently studying in M Kumarasamy College Of Engineering",
+};
+
+const introdata = {
+    title: "I’m Sanjayraj",
+    animated: {
+        first: "Devoted Data Enthusiast.",
+        second: "Dedicated Data Explorer.",
+        third: "Aspiring Data Wizard.",
+    },
+    description: "Enthusiastic B.tech Computer Science and Business Systems student with a focus on data analytics.",
+    your_img_url: "https://img.freepik.com/free-photo/business-concept-with-graphic-holography_23-2149160931.jpg?size=626&ext=jpg&ga=GA1.1.1342929832.1703916777&semt=ais",
+};
+
+const dataabout = {
+    title: "About My Self",
+    aboutme: "As a third-year B.Tech student majoring in Computer Science and Business Systems, my journey is fueled by a deep passion for data analytics. I thrive on the intersection of technology and business, seeking to unravel valuable insights from data to drive informed decision-making.",
+};
+const Internships = [{
+        InternRole: "Data Science",
+        where: "Slash Mark",
+        date: "2024",
+    },
+    {
+        InternRole: "Data Analyst",
+        where: "Meri SKILL",
+        date: "2023",
+    },
+    {
+        InternRole: "UI/UX Designing",
+        where: "CodSoft",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Sql",
+        value: 85,
+    },
+    {
+        name: "Power BI",
+        value: 80,
+    },
+    {
+        name: "Marketing analytics",
+        value: 85,
+    },
+    {
+        name: "Apache Hadoop",
+        value: 70,
+    },
+];
+
+const Achievements = [{
+        title: "Python",
+        description: "Participated in code debugging and won runner up in KSR Institute For Engineering And Technology, Tiruchengode.",
+    },
+    {
+        title: "Data Analytics",
+        description: "I have successfully completed the Data Analytics course on Coursera in Google Data Analytics Professional certificate program offered by Google.",
+    },
+    {
+        title: "UI & UX Design",
+        description: "Participated in UI/UX DESIGN workshop conducted by Sri Eswar College of Engineering and earned Best participation certificate.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://miro.medium.com/v2/resize:fit:1024/1*NKo_8Btk1-pIHSFVbaZCpQ.jpeg",
+        description: "Bus Management System.",
+        link: "https://github.com/Sanjayraj2003/Bus-Management-System",
+    },
+    {
+        img: "https://img.freepik.com/free-vector/mobile-marketing-concept-illustration_114360-1497.jpg?size=626&ext=jpg&ga=GA1.1.1342929832.1703916777&semt=ais",
+        description: "E-Marketing",
+        link: "https://github.com/Sanjayraj2003/E_market_master.html",
+    },
+    {
+        img: "https://e1.pxfuel.com/desktop-wallpaper/430/176/desktop-wallpaper-lemon-juice.jpg",
+        description: "Online Juice Order",
+        link: "https://www.figma.com/proto/4wKBIC5vCvYTF1ha5uNumb/Drinks?page-id=0%3A1&type=design&node-id=233-2&viewport=607%2C131%2C0.11&t=4Mz6oT6mn9mSSyuD-1&scaling=contain&mode=design",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sanjay300403@gmail.com",
+    YOUR_FONE: "(+91)7010047116",
+    description: "Feel free to reach out for collaboration, inquiries, or discuss about data analytics. I'm open to exploring opportunities, sharing insights, and connecting with like-minded professionals.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Sanjayraj2003",
+    facebook: "https://www.facebook.com/profile.php?id=100025103567388&mibextid=ZbWKwL",
+    linkedin: "https://www.linkedin.com/in/sanjayraj-c-997776245",
+    twitter: "https://x.com/Sanjayraj_?t=F9h34eM85aQs3SZKso5yGA&s=09",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    Internships,
+    skills,
+    Achievements,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/pages/about/index.js
@@ -0,0 +1,100 @@
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  dataabout,
+  meta,
+  Internships,
+  skills,
+  Achievements,
+} from "../../content_option";
+
+export const About = () => {
+  return (
+    <HelmetProvider>
+      <Container className="About-header">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> About | {meta.title}</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4">About me</h1>
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="5">
+            <h3 className="color_sec py-4">{dataabout.title}</h3>
+          </Col>
+          <Col lg="7" className="d-flex align-items-center">
+            <div>
+              <p>{dataabout.aboutme}</p>
+            </div>
+          </Col>
+        </Row>
+        <Row className=" sec_sp">
+          <Col lg="5">
+            <h3 className="color_sec py-4">Internships</h3>
+          </Col>
+          <Col lg="7">
+            <table className="table caption-top">
+              <tbody>
+                {Internships.map((data, i) => {
+                  return (
+                    <tr key={i}>
+                      <th scope="row">{data.InternRole}</th>
+                      <td>{data.where}</td>
+                      <td>{data.date}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="5">
+            <h3 className="color_sec py-4">Skills</h3>
+          </Col>
+          <Col lg="7">
+            {skills.map((data, i) => {
+              return (
+                <div key={i}>
+                  <h3 className="progress-title">{data.name}</h3>
+                  <div className="progress">
+                    <div
+                      className="progress-bar"
+                      style={{
+                        width: `${data.value}%`,
+                      }}
+                    >
+                      <div className="progress-value">{data.value}%</div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lang="5">
+            <h3 className="color_sec py-4">Achievements</h3>
+          </Col>
+          <Col lg="7">
+            {Achievements.map((data, i) => {
+              return (
+                <div className="Achievements_ py-4" key={i}>
+                  <h5 className="Achievements__title">{data.title}</h5>
+                  <p className="Achievements_desc">{data.description}</p>
+                </div>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
+    </HelmetProvider>
+  );
+};
